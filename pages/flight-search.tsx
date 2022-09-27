1adc15cd8777cd9269da2c60635eb5bdb3b42ec7
Brian Crawford <c110079@flyfrontier.com>
2022-09-27 09:40:37 -0600

    Bring in autocomplete

--- pages/flight-search.tsx
+++ pages/flight-search.tsx
@@ -1,12 +1,25 @@
 import { useRouter } from 'next/router';
+import InputAutocomplete from '../components/inputAutocomplete';
+import styles from '../styles/flight-search.module.scss';
 
 export default function FlightSearch() {
   const router = useRouter();
+  const suggestions: string[] = ['Bananas', 'Apples', 'Mangos'];
 
   return (
     <>
-      <div>Search Flights</div>
-      <button onClick={() => router.push('/flight-select')}>Search</button>
+      <h2>Search Flights</h2>
+      <div className={styles.container}>
+        <div className={styles.searchInputs}>
+          <label>From</label>
+          <InputAutocomplete suggestions={suggestions} id=""></InputAutocomplete>
+          <label>To</label>
+          <InputAutocomplete suggestions={suggestions} id=""></InputAutocomplete>
+        </div>
+        <div className={styles.searchButtonContainer}>
+          <button onClick={() => router.push('/flight-select')}>Search</button>
+        </div>
+      </div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import reportWebVitals from '../reportWebVitals';
 import Head from 'next/head';
 import FlightSearch from './flight-search';
+import styles from '../styles/index.module.scss';
 
 export const siteTitle = 'Low Fares Done Right | Frontier Airlines';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,14 +15,14 @@ export default function Home() {
         <meta name="og:title" content={siteTitle} />
       </Head>
       <FlightSearch></FlightSearch>
-    </>
+    </div>
   );
 }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
 
 // import Head from 'next/head'
 
